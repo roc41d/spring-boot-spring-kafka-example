dbd41ed86ae7b5d76f4a6f25d9024dad45c60877
Rocard <rocard@softtech-inc.com>
2021-02-24 12:14:29 +0100

    wip

---
+++ src/test/java/com/roc41d/springbootkafkaexample/SpringBootKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roc41d.springbootkafkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
